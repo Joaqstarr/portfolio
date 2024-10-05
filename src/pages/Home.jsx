0af5082d929a1a729c9f99d754bf60edc2aab4ea
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-10-05 16:00:46 -0400

    Implement Game Summary design and responsive behavior

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+﻿import TitleBar from "../Components/TitleBar";
+import PortfolioList from "../Components/PortfolioList";
+
+export default function Home() {
+    return (
+        <div>
+            <TitleBar />
+            {/**/}
+            <div className="ease-in duration-200 md:w-2/3 m-auto">
+                <PortfolioList/>
+
+            </div>
+        </div>
+    )
+}
