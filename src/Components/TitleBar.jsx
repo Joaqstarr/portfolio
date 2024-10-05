a88789d34986ab77dc5a97d1317397cf283df3d0
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-10-05 12:41:08 -0400

    Change file contents for appjs and titlebar

---
+++ src/Components/TitleBar.jsx
@@ -0,0 +1,9 @@
+function TitleBar(){
+    return(
+        <div className="w-full h-screen bg-amber-500 flex flex-col items-center justify-center">
+            <h1 className="text-9xl text-blue-100">Joaquin Royer</h1>
+        </div>
+    )
+}
+
+export default TitleBar;
