51112506d17cbc5a900e3b80b31ac89a942b3a01
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-10-19 19:51:05 -0400

    Convert subtitle id gen to use effect

--- src/Components/BlogParts/Subtitle.jsx
+++ src/Components/BlogParts/Subtitle.jsx
@@ -1,10 +1,16 @@
-﻿export default function Subtitle(params) {
+﻿import {useEffect, useState} from "react";
+
+export default function Subtitle(params) {
+    const [id, setId] = useState("");
+
+    useEffect( () => {
+        setId(CreateIdName(params.element.content));
+    }, [params.element.content]);
     return(
-        <h2 className="text-3xl font-bold mb-7 scroll-mt-48 lg:scroll-mt-5" id={CreateIdName(params.element.content)}  dangerouslySetInnerHTML={{__html: params.element.content}}></h2>
+        <h2 className="text-3xl font-bold mb-7 scroll-mt-48 lg:scroll-mt-5" id={id}  dangerouslySetInnerHTML={{__html: params.element.content}}></h2>
     )
 }
 
 function CreateIdName(text){
-    const replacedSpace = text.replace(/\s+/g, '-');
-    return replacedSpace;
+    return text.replace(/\s+/g, '-');
 }
