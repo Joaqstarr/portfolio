19293e08d33d57ba4b38a899d833211282909daa
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-10-19 03:53:38 -0400

    Fix image fullscreen + cmd wave json changes

--- src/Components/BlogParts/Image.jsx
+++ src/Components/BlogParts/Image.jsx
@@ -20,7 +20,9 @@ export default function Image(params) {
 
 function FullScreenImage({src, alt, isVisible}) {
     return (
-        <div className={`w-screen h-screen fixed bg-gray-500 inset-0 bg-opacity-80 flex justify-center items-center transition-opacity duration-200 cursor-default p-4 md:p-16 lg:p-32 ${isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none '}`}>
+        <div className={`w-screen h-screen fixed bg-gray-200 inset-0 flex-col bg-opacity-80 flex justify-center items-center transition-opacity duration-200 cursor-default p-4 md:p-16 lg:p-32 ${isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none '}`}>
+
+            <p className="mb-3 font-bold text-gray-900">{alt}</p>
             <img src={src} alt={alt} className={`max-w-full max-h-full w-full object-contain transition-transform duration-100 ease-in-out cursor-pointer ${isVisible ? 'scale-100' : 'scale-0'}`}></img>
         </div>
 
