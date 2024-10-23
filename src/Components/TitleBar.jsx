9b70665e3928f4c951b0ce0d1bf75b84b505b2ca
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-10-23 17:24:59 -0400

    Job Title Implement

--- src/Components/TitleBar.jsx
+++ src/Components/TitleBar.jsx
@@ -1,7 +1,8 @@
 function TitleBar(){
     return(
         <div className="w-full h-screen bg-amber-500 flex flex-col items-center justify-center sticky">
-            <h1 className="text-9xl text-blue-100">Joaquin Royer</h1>
+            <h1 className="text-8xl text-blue-100 mb-3 text-center font-bold">Joaquin Royer</h1>
+            <h2 className="text-4xl text-blue-100 text-center">Gameplay Programmer</h2>
         </div>
     )
 }
