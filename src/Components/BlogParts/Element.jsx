0a2b865470286a37e70c6b9c5705dc92eaf90425
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-10-23 17:30:16 -0400

    Time Display Component

--- src/Components/BlogParts/Element.jsx
+++ src/Components/BlogParts/Element.jsx
@@ -3,6 +3,7 @@ import Paragraph from "./Paragraph";
 import Subtitle from "./Subtitle";
 import Image from "./Image";
 import Video from "./Video";
+import ProjectTime from "./ProjectTime";
 
 export default function Element(params){
     switch (params.element.type){
@@ -16,6 +17,8 @@ export default function Element(params){
             return (<Subtitle element={params.element}/>);
         case "video":
             return (<Video element={params.element}/>);
+        case "time":
+            return (<ProjectTime element={params.element}/>);
 
     }
 }
---
+++ src/Components/BlogParts/ProjectTime.jsx
@@ -0,0 +1,5 @@
+﻿export default function ProjectTime(params) {
+    return(
+        <p className="mb-7 text-slate-900" ><strong>Project Time:</strong> {params.element.content}</p>
+    )
+}
