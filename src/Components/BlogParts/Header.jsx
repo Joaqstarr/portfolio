3b5baf77f3afa6536d4b706dd901247fe1d4ddd5
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-10-31 17:47:15 -0400

    Fix header overlapping with table of contents

--- src/Components/BlogParts/Header.jsx
+++ src/Components/BlogParts/Header.jsx
@@ -1,5 +1,5 @@
 ﻿export default function Header(params) {
     return(
-        <h1 className="text-7xl font-bold mb-3 font-header ramabhadra-regular z-10 pointer-events-none">{params.element.content}</h1>
+        <h1 className="text-7xl font-bold mb-3 font-header ramabhadra-regular pointer-events-none">{params.element.content}</h1>
     )
 }
