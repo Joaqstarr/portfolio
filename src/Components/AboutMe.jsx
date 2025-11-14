d5cb75a8e71d4d752a55dce10a5835930823bdfd
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-11-13 19:34:27 -0500

    Rename to Contact Me Button

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -11,19 +11,19 @@
                     <div>
                         <h2 className="text-5xl font-bold text-blue-100 mb-7">About Me</h2>
                         <p className="text-blue-100 max-w-screen-sm mb-4 text-xl italic">Hello!</p>
-                        <Paragraph>I am a junior Digital Media/Game Design Major at the University of Central Florida
+                        <Paragraph>I am a senior Digital Media/Game Design Major at the University of Central Florida
                             minoring in Computer Science. I have loved programming and developing games ever since I was
                             10 years old and I don't plan on stopping soon! I love solving complex problems through code
                             in readable and efficient ways. I have experience in both Unity and Unreal engine.</Paragraph>
-                        <Paragraph>I am also the president of the Game Dev Knights, the game
+                        <Paragraph>For 2 years, I was the president of the Game Dev Knights, the game
                             development club at UCF, where I educate and help people get started in Game
-                            Development. My favorite game series is The Legend of Zelda, but I play pretty much any game
+                            Development. My favorite game series is The Legend of Zelda, but I play pretty much any genre
                             in my free time.</Paragraph>
-                        <div className="flex flex-row mt-6 border-t-2 border-blue-100 border-dashed pt-4 gap-5">
-                            <IconButton link={githubLink} icon="fa-brands fa-github" text="Github"/>
-                            <IconButton link={itchLink} icon="fa-brands fa-itch-io" text="Itch"/>
-                            <IconButton link={resumeLink} icon="fa-file" text="Resume"/>
-                            <IconButton link={emailLink} icon="fa-envelope" text="Email"/>
+                        <div className="flex flex-row mt-6 border-t-2 border-blue-100 border-dashed pt-4 gap-5" newTab={true}>
+                            <IconButton link={githubLink} icon="fa-brands fa-github" text="Github" newTab={true}/>
+                            <IconButton link={itchLink} icon="fa-brands fa-itch-io" text="Itch" newTab={true}/>
+                            <IconButton link={resumeLink} icon="fa-file" text="Resume" newTab={true}/>
+                            <IconButton link={emailLink} icon="fa-envelope" text="Contact Me" newTab={false}/>
 
                         </div>
                     </div>
@@ -41,9 +41,9 @@
     )
 }
 
-function IconButton({link, icon, text}){
+function IconButton({link, icon, text, newTab}){
     return (
-        <a href={link} target="_blank" className="h-min text-blue-100 flex flex-col justify-center items-center hover:animate-bounce cursor-pointer">
+        <a href={link} target={newTab?"_blank":""} className="h-min text-blue-100 flex flex-col justify-center items-center hover:animate-bounce cursor-pointer">
             <i aria-hidden="true" className={`fa-solid text-3xl ${icon}`}/>
             <label className="cursor-pointer">{text}</label>
         </a>
